9fdfddc5cf16616bed3385949069c3224883bb60
Mir-Yuchi <mirazimov.kr@gmail.com>
2025-05-22 03:24:39 +0800

    feat(models): implemented User ORM model

--- app/models/user.py
+++ app/models/user.py
@@ -11,7 +11,7 @@ class User(Base):
     __table_args__ = (UniqueConstraint("email", name="uq_users_email"),)
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, nullable=False, unique=True, index=True)
-    hashed_password = Column(String, nullable=False)
+    password = Column(String, nullable=False)
     is_active = Column(Boolean, nullable=False, default=True)
     created_at = Column(
         TIMESTAMP(timezone=True), server_default=func.now(), nullable=False

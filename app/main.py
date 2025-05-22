f6c3eb980e03934b1410a10c5963d12b285cd28c
Mir-Yuchi <mirazimov.kr@gmail.com>
2025-05-23 03:10:25 +0800

    chore(main): include Borrow and Return  router in FastAPI app

--- app/main.py
+++ app/main.py
@@ -5,14 +5,16 @@ from sqlalchemy.orm import Session
 from app.core.config import settings
 from app.db.session import get_db
 from app.routers.book import router as book_router
+from app.routers.borrow import router as borrow_router
+from app.routers.reader import router as readers_router
 from app.routers.user import router as user_router
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
-from app.routers.reader import router as readers_router
 
 app.include_router(user_router)
 app.include_router(book_router)
 app.include_router(readers_router)
+app.include_router(borrow_router)
 
 
 @app.get("/health/db")

54e8c4a345fb870d44538a69272b9bb6f36a993a
Mir-Yuchi <mirazimov.kr@gmail.com>
2025-05-23 02:17:47 +0800

    chore(main): include reader router in FastAPI app

--- app/main.py
+++ app/main.py
@@ -8,9 +8,11 @@ from app.routers.book import router as book_router
 from app.routers.user import router as user_router
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
+from app.routers.reader import router as readers_router
 
 app.include_router(user_router)
 app.include_router(book_router)
+app.include_router(readers_router)
 
 
 @app.get("/health/db")
